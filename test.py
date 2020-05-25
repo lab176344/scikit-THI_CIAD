5da8af1fda463b50d487e317fa700308881b8b6c
Balasubramanian <Lakshman.Balasubramanian@carissma.eu>
2020-05-25 11:15:56 +0200

    testing file

---
+++ test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 11:12:17 2020
+
+@author: balasubramanian
+"""
+test_pass = False
+try:
+    from sklearn.ensemble import RandomForestClassifier
+    test_pass = True
+except  ImportError as err:
+    print('There is problem in either tree.pyx or _classes.py')
+    
+try: 
+    from from sklearn.ensemble import _indexing_tree
+    test_pass = True
+except ImportError as err:
+    print('There is problem in with the indexing')
+
+
+if test_pass:
+    print('test pass')
